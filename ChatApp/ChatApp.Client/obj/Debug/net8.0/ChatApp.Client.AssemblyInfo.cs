0d5f04e0187c455981d5872cae0a48a78ef46b36
Furkan Ekici <furekici.06@gmail.com>
2024-08-27 01:16:35 +0300

    .

--- ChatApp/ChatApp.Client/obj/Debug/net8.0/ChatApp.Client.AssemblyInfo.cs
+++ ChatApp/ChatApp.Client/obj/Debug/net8.0/ChatApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8faebf9cbfb0087ae816bb5fa19ebaf3882c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChatApp/ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8faebf9cbfb0087ae816bb5fa19ebaf3882c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
